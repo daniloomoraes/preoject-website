f43ae68c5898429449472be1277ddabc64582ec2
Danilo Oliveira <danilo.oliveira@driven.cx>
2021-10-16 21:34:10 -0300

    Ajustes page details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 
 import Container from '../components/container'
 import Footer from '../components/footer'
@@ -6,19 +7,26 @@ import Header from '../components/header'
 
 import '../assets/styles/details.scss';
 
-const Details = (props) => (
+
+const Details = (props) => {
+
+  if (!props.location.Name) {
+    return  <Redirect  to="/portifolio" />
+  }
+  return(
     <>
-        <Header />
-        <Container>
-            <h1>{props.location.Name}</h1>
-            <img src={props.location.urlImage} className="img" alt="" />
-            <p className="description">{props.location.Description}</p>
-            <a href={props.location.urlLink} target="_blank" rel="noopener noreferrer" className="button">Visit</a>
-            <a href="/portifolio" className="button">Back</a>
-            <a href="/favorites" className="button">Favorite</a>
-        </Container>
-        <Footer />
+      <Header />
+      <Container>
+        <h1>{props.location.Name}</h1>
+        <img src={props.location.urlImage} className="img" alt="" />
+        <p className="description">{props.location.Description}</p>
+        <a href={props.location.urlLink} target="_blank" rel="noopener noreferrer" className="button">Visit</a>
+        <a href="/portifolio" className="button">Back</a>
+        <a href="/favorites" className="button">Favorite</a>
+      </Container>
+      <Footer />
     </>
-)
+  )
+}
 
 export default Details
